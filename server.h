9958ba84f5f50e272ea122ca5aea9e561a1381d7
Gardakan <vpailhe@student.42.fr>
2015-05-10 00:56:10 +0200

    Update server.h

--- server.h
+++ server.h
@@ -12,12 +12,6 @@
 
 #ifndef SERVER_H
 # define SERVER_H
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <netinet/in.h>
-# include <arpa/inet.h>
+# include "ft_p.h"
 
 #endif
