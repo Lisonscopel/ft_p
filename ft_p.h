5474bd5527e63e38c8221b1ff46d9ed617a149c1
Lison SCOPEL <lscopel@e1r12p5.42.fr>
2015-05-10 00:45:16 +0200

    server.h, ft_p.h

---
+++ ft_p.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_p.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lscopel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/09 23:03:19 by lscopel           #+#    #+#             */
+/*   Updated: 2015/05/10 00:44:31 by lscopel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_P_H
+# define FT_P_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+#endif
---
+++ server.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lscopel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/09 22:56:02 by lscopel           #+#    #+#             */
+/*   Updated: 2015/05/10 00:44:41 by lscopel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+#endif
