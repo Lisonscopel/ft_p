2dbad8d18e4fa21fe27fb9d826a285a80d10d782
Vincent PAILHE <vpailhe@e1r12p5.42.fr>
2015-05-20 18:28:02 +0200

    first step

--- src/client/client.c
+++ src/client/client.c
@@ -1,5 +1,30 @@
 #include "client.h"
 
+int		login(int sock)
+{
+	char	buff[1024];
+	char	*line;
+	char	*line2;
+
+	ft_putendl("yo");
+
+	send(sock, "okcbon", 6, 0);
+
+	ft_putendl("Login:");
+	get_next_line(0, &line);
+	ft_putendl("Password:");
+	get_next_line(0, &line2);
+	line = ft_strjoin(line, ":");
+	line = ft_strjoin(line, line2);
+
+	recv(sock, buff, 1023, 0);
+
+	send(sock, line, ft_strlen(line), 0);
+
+	ft_putendl(line);
+	return (0);
+}
+
 static void	prompt(int sock)
 {
 	char	buff[1024];
@@ -27,6 +52,8 @@ int			main(int ac, char **av)
 		ft_usage(av[0], " <addr> <port>");
 	port = ft_atoi(av[2]);
 	sock = client_create_tcp(av[1], port);
+	if (login(sock))
+		return (0);
 	while (42)
 	{
 		prompt(sock);
--- src/server/server.c
+++ src/server/server.c
@@ -6,20 +6,31 @@
 /*   By: ghilbert <ghilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/09 17:22:10 by sduprey           #+#    #+#             */
-/*   Updated: 2015/05/17 16:46:53 by vpailhe          ###   ########.fr       */
+/*   Updated: 2015/05/20 18:27:06 by vpailhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
-/*
-static void		prompt_display(int fd)
-{
-	char		*pwd;
 
-	pwd = ft_strjoin(pwd_handler(), " > ");
-	ft_putstr_fd(pwd, fd);
+int						server_login(int sock)
+{
+	int		ret = 0;
+	char	buff[1024];
+
+	while ((ft_strcmp(buff, "okcbon"))) 
+	{
+		ret = recv(sock, buff, 1023, 0);
+		buff[ret] = 0;
+	}
+	ft_putendl("A");
+	send(sock, "wfyl", 5, 0);
+	ret = recv(sock, buff, 1023, 0);
+	buff[ret] = 0;
+	ft_putendl(buff);
+	//ft_strcmp(buff, )
+	return (0);
 }
-*/
+
 int						main(int ac, char **av)
 {
 	int					port;
@@ -38,6 +49,8 @@ int						main(int ac, char **av)
 //		prompt_display(cs);
 		if (fork()  == 0)
 		{
+			if (!(server_login(sock)))
+				exit(0);
 			dial_client(cs);
 			close(cs);
 			exit(0);
