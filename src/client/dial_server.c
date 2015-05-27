6f2d4b9088df915ce685202faeafa036c694d36f
Vincent PAILHE <vpailhe@e1r11p6.42.fr>
2015-05-27 15:05:29 +0200

    mult get in da place

--- src/client/dial_server.c
+++ src/client/dial_server.c
@@ -1,33 +1,10 @@
 #include "client.h"
 
-
-void	dial_server(int sock, char *line)
+void		no_action(int sock)
 {
-	t_fct	*list;
-	int		bool;
-	t_fct	*tmp;
-	char	**new_av;
-	int		ret_val;
-	char	buf[32768];
 	int		ret;
+	char	buf[32768];
 
-	bool = 0;
-	init(&list);
-	tmp = list;
-	new_av = ft_strsplit_blank(line);
-	send(sock, line, ft_strlen(line), 0);
-	while (tmp != NULL)
-	{
-		if (new_av[0] && ft_strcmp(tmp->name, new_av[0]) == 0)
-		{
-			ret_val = tmp->ptr_funct(new_av, sock);
-			bool = 1;
-			break ;
-		}
-		tmp = tmp->next;
-	}
-	if (bool == 0)
-	{
 		while ((ret = recv(sock, buf, 32767, 0)))
 		{
 			buf[ret] = '\0';
@@ -44,5 +21,56 @@ void	dial_server(int sock, char *line)
 			if (ret < 32767)
 				break ;
 		}
+}
+
+int			mult_get(char *name, char **t)
+{
+	if (ft_strcmp(name, t[0]))
+		return (0);
+	if (t[2])
+		return (1);
+	return (0);
+}
+
+char		*join_cmd(char **t)
+{
+	char	*rtn;
+	int		i;
+
+	i = 2;
+	while(t[i])
+	{
+		rtn = ft_strjoin(rtn, " ");
+		rtn = ft_strjoin(rtn, t[i]);
+		i++;
 	}
+	return (rtn);
+}
+
+void		dial_server(int sock, char *line)
+{
+	t_fct	*list;
+	int		bool;
+	t_fct	*tmp;
+	char	**new_av;
+
+	bool = 0;
+	init(&list);
+	tmp = list;
+	new_av = ft_strsplit_blank(line);
+	send(sock, line, ft_strlen(line), 0);
+	while (tmp != NULL)
+	{
+		if (new_av[0] && ft_strcmp(tmp->name, new_av[0]) == 0)
+		{
+			tmp->ptr_funct(new_av, sock);
+			if (mult_get(tmp->name, new_av))
+				dial_server(sock, join_cmd(new_av));
+			bool = 1;
+			break ;
+		}
+		tmp = tmp->next;
+	}
+	if (bool == 0)
+		no_action(sock);
 }
